90657f078edf1d5a119dffaa1730d9bd1195e936
Drew Alam <dhruva.alam@gmail.com>
2017-06-18 13:10:30 -0400

    2.5

---
+++ C2-LinkedLists/2.5/2.5/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  2.5
+//
+//  Created by Drew Alam on 2017-06-18.
+//  Copyright © 2017 Drew Alam. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
